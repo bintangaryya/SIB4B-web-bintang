d47a953a5e8ca0225da3bec2000efaaecfd8b920
bintangaryya <bintang.aryya1@gmail.com>
2026-04-30 12:17:54 +0800

    lanjutan

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,3 +1,15 @@
 export default function LearnPage() {
-    return <p>Kita akan belajar disini</p>;
+    return (
+        <div className="bg-purple-300" width="500">
+            <div className="bg-orange-300">
+                <img/>
+                <p> Design </p>
+            </div>
+
+            <div>
+                <h1> Figma </h1>
+                <p> 8 Hours Ago </p>
+            </div>
+        </div>
+    )
 }
